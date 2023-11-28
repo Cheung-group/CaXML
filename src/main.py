08fcc303c2d3251069930715f2f75578bf9d557a
Pengzhi Zhang <pzhang.uh@gmail.com>
2023-11-28 00:03:01 -0600

    Refactor code to improve performance and readability

--- src/main.py
+++ src/main.py
@@ -1,25 +1,39 @@
 # Main code to extract network parameters from pdb structures
 
-import glob
 import sys
 import argparse
+from tqdm import tqdm
+from pathlib import Path
+
 from Bio.PDB import PDBParser
-from .graph_feature_extraction import calc_dist_matrix, loop_dist_matrix, contact, network
-from .symmetry_funcs import Structure
+from graph_feature_extraction import calc_dist_matrix, loop_dist_matrix, contact, network
+from symmetry_funcs import Structure
+
+import warnings
+from Bio import BiopythonWarning
+warnings.simplefilter('ignore', BiopythonWarning)
+
 
 parser = argparse.ArgumentParser(description='Extract network parameters from pdb structures')
-parser.add_argument('dir', metavar='DIR', type=str, help='directory containing pdb files')
+parser.add_argument('dir', metavar='DIR', type=str, help='directory containing pdb files (pdb files with extension .pdb))')
 args = parser.parse_args()
 
-allpdb = glob.glob(args.dir + "/*.pdb")
+
+pdbdir = Path(args.dir)
+allpdb = list(pdbdir.glob("*.pdb"))
+count_total = len(allpdb)
+
+
+# Create network and extract network parameters
 deg_centrality = []
 bet_centrality = []
 close_centrality = []
 cluster_coeff = []
 
 counter = 0
-for struct in allpdb:
-    pdb_code = struct.split('/')[-1].split('.pdb')[0]
+for struct in tqdm(allpdb, total=count_total):
+    ###pdb_code = struct.split('/')[-1].split('.pdb')[0]
+    pdb_code = struct.stem
     structure = PDBParser().get_structure(pdb_code, struct)
     dist_matrix = calc_dist_matrix(structure,structure, method='heavy')
 
@@ -33,27 +47,30 @@ for struct in allpdb:
     close_centrality.append(cce)
     cluster_coeff.append(cco)
 
-    print(f"{pdb_code},{dc},{bc},{cce},{cco}")
+    #print(f"{pdb_code},{dc},{bc},{cce},{cco}")
     counter = counter + 1
-    if counter % 100 == 0:
-        print(counter, "structures processed ...", end = '\r')
-        sys.stdout.flush()
+    #if counter % 100 == 0:
+    #    print(counter, "structures processed ...", end = '\r')
+    #    sys.stdout.flush()
 
 
 
+# calculate symmetry parameters
 # Use all the files
 loops = allpdb
 
 structures = dict()
 counter = 0
-count_total = len(loops)
-for loop in loops:
-    name = loop.split('/')[1].split('.pdb')[0]
+
+for loop in tqdm(loops, total=count_total):
+    #print(loop)
+    #name = loop.split('/')[-1].split('.pdb')[0]
+    name = loop.stem
     structures[name]=Structure(loop)
     structures[name].calc_sym()  
 
     counter = counter + 1
-    if counter % 100 == 0:
-        #sys.stdout.write('\r')
-        print(counter, "structures out of ", count_total, " processed ...", end = '\r')
-        sys.stdout.flush()
+    # if counter % 100 == 0:
+    #     #sys.stdout.write('\r')
+    #     print(counter, "structures out of ", count_total, " processed ...", end = '\r')
+    #     sys.stdout.flush()
--- src/symmetry_funcs.py
+++ src/symmetry_funcs.py
@@ -1,20 +1,27 @@
 import numpy as np
 from ase.io import read
 from dscribe.descriptors import ACSF
+from pathlib import Path
 
 
+# Atomic number to element symbol
 aN={1:'H',6:'C',7:'N',8:'O',20:'Ca'}
+elements = ['H','C','N','O','Ca']
 
 class Structure:
+    """
+    The class takes the pdb file and extract the symmetry functions.
+    
+    Parameters
+    ----------
+    pdb_path : Path
+        The path to the pdb file.
+    """
     
     def __init__(self, pdb_path):
-        self.name = pdb_path.split('/')[1].split('.pdb')[0]
-        self.water = int(self.name.split('.')[2][5:])
-        self.system = self.name.split('.')[0]
-        self.loop = self.name.split('.')[1]
-        self.mdframe = self.name.split('.')[3]
-        self.feat_atom=dict()
-        #print("PDB name is ", self.name)
+        self.pdb_path = Path(pdb_path) if isinstance(pdb_path, str) else pdb_path
+        self.name = pdb_path.stem
+        # self.feat_atom = dict()
         
     def calc_sym(self):
         '''
@@ -24,51 +31,59 @@ class Structure:
                 Length of the list = number of atoms
             UPDATE: remove water molecules.
         ''' 
-        pdb_file = self.name+'.pdb'
-        chg_file = self.name + '.chg'
-        structure = read("pdb/"+pdb_file)
+
+
+        # Read the pdb file using ase.io.read
+        # PZhang: For some reason (format of pdb?), some atom symbols are not correct if not provided.
+        # example: out_5cpv.pdb_match_4.pdb
+        # Cd, Ce, Og, are not recognized by ASE as C, C, and O.
+        # Atoms(symbols='C77Ca29Cd15Ce8N32O41Og4', pbc=False, atomtypes=..., bfactor=..., occupancy=..., residuenames=..., residuenumbers=...)
+        # To avoid this problem, we provide the element symbols in columns 77 and 78 of the pdb file.
+        ## From the ase code:
+            # if line.startswith('ATOM') or line.startswith('HETATM'):
+            # Atom name is arbitrary and does not necessarily
+            # contain the element symbol.  The specification
+            # requires the element symbol to be in columns 77+78.
+
+        structure = read(self.pdb_path)
         feat_lst=[]
         num=structure.get_global_number_of_atoms()
         atomPos = [x for x in range(0,num)]
-
-        chg=np.genfromtxt("charge/"+chg_file)
-
 
         #for r_cut in [x/4. for x in range(4,13,1)]:
          
         # Setting up the ACSF descriptor
         for r_cut in [4.]:
             acsf = ACSF(
-                species=["C", "H", "O", "N", "Ca"],
-                rcut=r_cut,
+                species=elements,
+                r_cut=r_cut,
                 g2_params=[[0.0001,0]],
             )
-            feat_lst.append(acsf.create(structure,positions=atomPos,n_jobs=4))
+            feat_lst.append(acsf.create(structure, centers=atomPos, n_jobs=4))
 
         acsf_ang = ACSF(
-            species=["C", "H", "O", "N", "Ca"],
-            rcut=6.0,
+            species=elements,
+            r_cut=6.0,
             g4_params=[[0.0001,0.5,-1]],
         )
-        feat_lst.append(acsf_ang.create(structure,positions=atomPos,n_jobs=4))
+        feat_lst.append(acsf_ang.create(structure, centers=atomPos, n_jobs=4))
 
         feat = np.hstack(tuple(feat_lst))
-        dat = np.hstack((feat,chg))
-        np.savetxt('sym/sym_'+pdb_file[0:-4]+'.dat', dat, delimiter=',')
+        np.savetxt(f'sym_{self.name}.csv', feat, delimiter=',')
         
-        # Skip water here
-        # Skip to the last water x 3 rows
-        skip_water = -3 * self.water
-        # If there is no water, skip none (-1)
-        if skip_water == 0:
-            skip_water = None
-        #print("skip water atoms",skip_water)
-        #print(dat.shape)
-        #print(dat[:skip_water,:].shape)
-        for i in dat[:skip_water,:]:
-            ikey = aN[int(i[-2])]
-            if ikey in self.feat_atom:
-                self.feat_atom[ikey] = np.vstack((self.feat_atom[ikey],i))
-            else:
-                #print(ikey,"is not in the dictionary, adding ...")
-                self.feat_atom[ikey] = i
+        # # Skip water here
+        # # Skip to the last water x 3 rows
+        # skip_water = -3 * self.water
+        # # If there is no water, skip none (-1)
+        # if skip_water == 0:
+        #     skip_water = None
+        # #print("skip water atoms",skip_water)
+        # #print(dat.shape)
+        # #print(dat[:skip_water,:].shape)
+        # for i in dat[:skip_water,:]:
+        #     ikey = aN[int(i[-2])]
+        #     if ikey in self.feat_atom:
+        #         self.feat_atom[ikey] = np.vstack((self.feat_atom[ikey],i))
+        #     else:
+        #         #print(ikey,"is not in the dictionary, adding ...")
+        #         self.feat_atom[ikey] = i
